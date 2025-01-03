2fe3000219cdf11fc1d5973234fcda12e25f9255
EvanNoNo <gonzalezevans05@gmail.com>
2025-01-03 14:18:27 -0500

    code menu, css, js

--- index.php
+++ index.php
@@ -1,11 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Evan González</title>
+    <link rel="stylesheet" href="css/estiloGlobal.css">
+    <link rel="stylesheet" href="./css/menu.css">
+    <link rel="shortcut icon" href="img/3_icon-black.svg" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 </head>
-<body>
-    
-</body>
+</head>
+
+<body class="holy-grail">
+
+		<header>
+			
+            <?php 
+            include ("./menu_footer/menu.html");
+            ?>
+		</header>
+
+		<div class="holy-grail-body">
+
+			<section class="holy-grail-content">
+				<p>Main Content</p>
+			</section>
+
+			<div class="holy-grail-sidebar-1 hg-sidebar">
+				<p>Sidebar 1</p>
+			</div>
+
+			<div class="holy-grail-sidebar-2 hg-sidebar">
+				<p>Sidebar 2</p>
+			</div>
+
+		</div>
+
+		<footer>
+			<p>Footer</p>
+		</footer>
+	</body>
+
 </html>
